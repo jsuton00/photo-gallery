7a60b6dcc8f2b01f6fd392e22d0d4083673369b7
Jozo Suton <61884667+jsuton00@users.noreply.github.com>
2022-10-27 15:42:26 +0200

    header background image changed

--- src/components/HeaderPhoto.jsx
+++ src/components/HeaderPhoto.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import heroImage from '../assets/beach.jpg';
+import heroImage from '../assets/photography.jpg';
 
 const HeaderPhoto = () => {
-	return <img src={heroImage} alt="beach" className="header-photo" />;
+	return <img src={heroImage} alt="photogrpahy" className="header-photo" />;
 };
 
 export default HeaderPhoto;
